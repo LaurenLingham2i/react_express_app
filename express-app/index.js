fef9ccc1888debb400c84fa7c7fbcaa6394a8b20
LaurenLingham2i <lauren.lingham@2itesting.com>
2023-11-16 16:20:50 +0000

    top 10 articles displaying

--- express-app/index.js
+++ express-app/index.js
@@ -6,35 +6,38 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Deck of Cards API");
-})
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the Deck of Cards API");
+// })
 
-app.get("/api/deck", async (req, res) => {
-  try {
-    const response = await axios.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-    const deckId = response.data.deck_id;
-    res.json({ message: `Deck ID: ${deckId}` });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error"});
-  }
-});
-
-// app.get("/", async (req, res) => {
+// app.get("/api/deck", async (req, res) => {
 //   try {
-//     const response = await axios.get("https://content.guardianapis.com/search?show-fields=all&api-key=647eea2e-e426-4a85-9fc7-4466c09b7447");
-//     console.log(response);
-//     // console.log(response.data.response.results[0].webTitle);
-
-
-//     // const firstArticle = response.results[1].id;
-//     // res.json({ message: `First article: ${firstArticle}` });
+//     const response = await axios.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+//     const deckId = response.data.deck_id;
+//     res.json({ message: deckId });
 //   } catch (error) {
 //     console.error(error);
 //     res.status(500).json({ error: "Internal server error"});
 //   }
 // });
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://content.guardianapis.com/search?show-fields=all&api-key=647eea2e-e426-4a85-9fc7-4466c09b7447");
+    const topTenArticles = response.data.response.results;
+        res.json({ message: topTenArticles });
+  } catch (error) {
+
+    // let details = article.fields;
+    //     let headline = details.headline;
+    //     let image = details.thumbnail;
+    //     let link = shortUrl;
+    //     let key = top10.indexOf(song);
+
+    console.error(error);
+    res.status(500).json({ error: "Internal server error"});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,29 +3,30 @@ import './App.css';
 import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    console.log("Fetching data...")
-    axios.get("http://localhost:5000/api/deck")
-      // .then(response => response.json())
-      .then(response => {
-        console.log("Data received: ", response);
-        setMessage(response.data.message);
+    console.log("Fetching data...");
+    axios.get("http://localhost:5000/")
+      .then((response) => {
+        console.log(response.data);
+        setArticles(response.data.message);
       })
-      .catch(error => console.error("Error fetching message: ", error));
+      .catch(error => console.error(`Error fetching message: ${error}`)); 
   }, []);
 
   return (
-    <>
-      <div className="App">
-        <header className="App=header">
-          <p>Test</p>
-          <p>{message}</p>
-        </header>
-      </div>
-    </>
+    <div className="App">
+      <p>Test</p>
+      {articles.map((article, index) => (
+        <div key={index}>
+          <p>{article.fields.headline}</p>
+          <img src={article.fields.thumbnail} alt="" />
+        </div>
+      ))}
+    </div>
   );
 }
+
 
 export default App;
